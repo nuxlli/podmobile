c895b73aeff25b301110779d52773d27c8f40dc1
Everton Ribeiro <nuxlli@gmail.com>
2009-02-05 20:58:36 -0200

    Limpando arquivos desnecessários e movendo algumas classes de lugar

--- eibox/page.py
+++ eibox/page.py
@@ -5,30 +5,5 @@ import logging, imp, os
 from PyQt4 import QtWebKit
 
 class EiboxWebPage(QtWebKit.QWebPage):
-
-    def __init__(self, parent, window):
-        QtWebKit.QWebPage.__init__(self, parent)
-        self.window = window
-
     def javaScriptConsoleMessage(self, message, line, source):
         logging.error("Javascript(%d): %s - %s" % (line, message, source))
-
-    def createPlugin(self, classId, QUrl, paramNames, paramValues):
-        params = {}
-        i = 0
-        for name in paramNames:
-            params[str(name)] = str(paramValues[i])
-            i += 1
-
-        classId = str(classId.toUtf8())
-
-        logging.debug(classId)
-
-        for folder in self.window.settings.PLUGINS:
-            filename = os.path.join(folder, classId.lower() + ".py")
-            if os.path.exists(filename):
-                #try:
-                module = imp.load_source(classId.lower(), filename)
-                return eval("module.%s(self.window)" % classId)
-
-        return None
